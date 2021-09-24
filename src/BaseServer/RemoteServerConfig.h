e5b90df5fbc74d84d215ec04a27eb6e8c721ef8a
Meghtar <maciej.pypec@gmail.com>
2021-09-24 19:40:39 +0200

    Added .gitignore and missing a missing file

---
+++ src/BaseServer/RemoteServerConfig.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+
+struct RemoteServerConfig
+{
+    RemoteServerConfig(const std::string& host, const uint16_t port) :
+        _remoteServicePort(port), _remoteServiceHost(host), _useRemote(true) 
+    {}
+    RemoteServerConfig() :
+        _remoteServicePort(0), _remoteServiceHost(""), _useRemote(false) 
+    {}
+    uint16_t getPort() const { return _remoteServicePort; }
+    std::string getHost() const { return _remoteServiceHost; }
+    bool useRemote() const { return _useRemote; }
+private:
+    const std::string _remoteServiceHost;
+    const uint16_t _remoteServicePort;
+    const bool _useRemote;
+};
